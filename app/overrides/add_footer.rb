25dec4454d69218896335274c73126b74d37bc31
Quy Doan <quydoantran@gmail.com>
2012-03-22 13:55:23 +0800

    clean up code

--- app/overrides/add_footer.rb
+++ app/overrides/add_footer.rb
@@ -1,4 +1,9 @@
 Deface::Override.new(:virtual_path => %q{spree/layouts/spree_application},
+                          :name => %q{remove_all_footer},
+                          :remove => %q{footer#footer})
+
+Deface::Override.new(:virtual_path => %q{spree/layouts/spree_application},
                           :name => %q{add_footer},
                           :insert_bottom => %q{[data-hook='body']},
                           :partial => 'spree/shared/footer')
+
