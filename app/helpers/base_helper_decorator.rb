5938a52f295a58467ccd9e352477856cc833ef0f
Vivek Aditya <vivekg342@gmail.com>
2012-05-28 19:01:15 +0530

    Ovveride layout completely
    Notifications

---
+++ app/helpers/base_helper_decorator.rb
@@ -0,0 +1,16 @@
+Spree::BaseHelper.class_eval do
+  def flash_messages
+    [:notice, :error].map do |msg_type|
+      cls  = msg_type=="notice" ? "alert-info" : "alert-block alert-error"
+      if flash[msg_type]
+        content_tag :div , :class => "alert #{cls}" do
+          content_tag(:a, "x", :class => "close", "data-dismiss" => "alert") +
+          content_tag(:p, flash[msg_type])
+        end
+      else
+        ''
+      end
+    end.join("\n").html_safe
+  end
+
+end
