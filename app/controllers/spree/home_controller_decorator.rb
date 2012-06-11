aa8f8c97f4ca73fe55d33bddd683a33edec415cd
Vivek Aditya <vivekg342@gmail.com>
2012-06-11 11:20:42 +0530

    Beginner auctions

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,7 +2,10 @@ module Spree
   HomeController.class_eval do
     def index
       @flash_sales = Spree::FlashSale.includes([:variant]).by_state(['reserved','live','pending','empty']).order('end_date desc').limit(4)
-      @auctions = Spree::Auction.includes([:variant]).pending_and_live_auction
+      @auctions = Spree::Auction.includes([:variant]).general.pending_and_live_auction
+      if current_user and current_user.beginner?
+        @beginner_auctions = Spree::Auction.includes([:variant]).beginner.pending_and_live_auction.all(:limit => 2)
+      end
     end
   end
 end
