d95a6900114af175ce9f31b723c6a41b923c9403
Vivek Aditya <vivekg342@gmail.com>
2012-06-25 00:38:12 +0800

    Pagination changes

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,7 +2,7 @@ module Spree
   HomeController.class_eval do
     def index
       @flash_sales = Spree::FlashSale.includes([:variant]).by_state(['reserved','live','pending','empty']).order('end_date desc').limit(4)
-      @auctions = Spree::Auction.includes([:variant]).general.pending_and_live_auction
+      @auctions = Spree::Auction.includes([:variant]).general.pending_and_live_auction.page(1).per(6).order("remaining_time asc")
       if current_user and current_user.beginner?
         @beginner_auctions = Spree::Auction.includes([:variant]).beginner.pending_and_live_auction.all(:limit => 2)
       end
