5ff71c38ba4de4d252d5daf3b171c304540c1f4d
Vivek Aditya <vivekg342@gmail.com>
2012-06-25 11:56:19 +0800

    Fix flash sales alignment beginner auctions display align sub navigation

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,9 +2,13 @@ module Spree
   HomeController.class_eval do
     def index
       @flash_sales = Spree::FlashSale.includes([:variant]).by_state(['reserved','live','pending','empty']).order('end_date desc').limit(4)
-      @auctions = Spree::Auction.includes([:variant]).general.pending_and_live_auction.page(1).per(6).order("remaining_time asc")
-      if current_user and current_user.beginner?
+      if current_user
+        if current_user.beginner?
         @beginner_auctions = Spree::Auction.includes([:variant]).beginner.pending_and_live_auction.all(:limit => 2)
+        end
+        @auctions = Spree::Auction.includes([:variant]).general.pending_and_live_auction.page(1).per(6).order("remaining_time asc")
+      else
+        @auctions = Spree::Auction.includes([:variant]).pending_and_live_auction.page(1).per(6).order("remaining_time asc")
       end
     end
   end
