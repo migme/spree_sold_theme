5c4057ae845c042014665d6572219c406d841872
jojith.rajan <jojith.rajan@revenuemed.com>
2012-06-30 11:30:49 +0530

    Overrides are taken out.

--- app/overrides/replace_cart_form.rb
+++ app/overrides/replace_cart_form.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => %q{spree/products/show},
-                          :name => %q{replace_cart_form},
-                          :replace => %q{code:contains("render :partial => 'cart_form'")},
-                          :text => %q{<code erb-loud>render(:partial => 'sale_cart_form')</code>})
+#Deface::Override.new(:virtual_path => %q{spree/products/show},
+#                          :name => %q{replace_cart_form},
+#                          :replace => %q{code:contains("render :partial => 'cart_form'")},
+#                          :text => %q{<code erb-loud>render(:partial => 'sale_cart_form')</code>})
