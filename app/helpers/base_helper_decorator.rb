414be1ba5cbc14b4cba048ddc5baf1c939709879
Donald John Nakar <donald.nakar@gmail.com>
2013-06-10 16:55:45 +0800

    Update tool bar links color to yellow

--- app/helpers/base_helper_decorator.rb
+++ app/helpers/base_helper_decorator.rb
@@ -55,18 +55,15 @@ Spree::BaseHelper.class_eval do
     if current_user and current_user.has_role?("auction_user") || current_user.has_role?("admin")
 
       if current_user.beginner?
-        @auction_notifications <<  "Welcome #{current_user.first_name.capitalize}! New to Sold.sg? #{link_to("Click here", "http://feedback.sold.sg/knowledgebase/topics/3851-auction")} for our FAQs!"
+        @auction_notifications <<  "Welcome #{current_user.first_name.capitalize}! New to Gamers.ph? #{link_to("Click here", "http://feedback.gamers.ph/knowledgebase/topics/3851-auction")} for our FAQs!"
 
-        @auction_notifications <<  "#{link_to("Click here", "http://feedback.sold.sg/knowledgebase/articles/112887-tips-and-strategies-basic-bidding-strategies")} for Bidding Tips & Strategies! "
+        @auction_notifications <<  "#{link_to("Click here", "http://feedback.gamers.ph/knowledgebase/articles/112887-tips-and-strategies-basic-bidding-strategies")} for Bidding Tips & Strategies! "
       end
 
 
       @auction_notifications <<  "#{link_to("Click here", completed_auctions_url(subdomain: 'auctions'))} to see what  you have missed!"
 
-
-      @auction_notifications <<  "Hi #{current_user.first_name.capitalize}! Did you know we are certified by <a target='_blank' href='http://www.cnsg.com.sg/accreditation/Soldgers.pdf'>TrustSg</a>" # and <a href='javascript:vrsn_splash()' tabindex='-1'>Verisign</a>"
-
-      @auction_notifications << "Free Shipping applies to purchases of $150 or more (excl.Token Packs)."
+      @auction_notifications << "Free Shipping applies to purchases of Php 3,000.00 or more (excl.Token Packs)."
 
 
       auctions = Spree::Auction.by_state('live').where(["remaining_time < ?", 10.minutes.from_now.utc.to_i]).order('remaining_time')
