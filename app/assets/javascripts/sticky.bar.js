34e82db04af6586a57689f522d696912b714b56d
Quy Doan <quydoantran@gmail.com>
2013-06-25 19:02:21 +0800

    remove console.info in stickybar.

--- app/assets/javascripts/sticky.bar.js
+++ app/assets/javascripts/sticky.bar.js
@@ -4,9 +4,7 @@
         this.each(function(){
             var $this = $(this);
             var barOffset = $this.offset().top;
-            console.info("A:" + barOffset);
             $(document).on('scroll', function(){
-                console.info("B:" + barOffset);
                 if ($(document).scrollTop() > barOffset){
                     $this.addClass('sticky-bar');
                 }else{
